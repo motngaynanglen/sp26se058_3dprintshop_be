76b8ea3006c2cefcde2df5670c0aba69285f7189
bachdoan-sa <bach.doansy@gmail.com>
2026-01-14 02:30:57 +0700

    update database customer, init migration, database

--- src/Domain/Common/BaseEntity.cs
+++ src/Domain/Common/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace sp26se058_3dprintshop_be.Domain.Common;
 
@@ -7,8 +8,11 @@ public abstract class BaseEntity
     // This can easily be modified to be BaseEntity<T> and public T Id to support different key types.
     // Using non-generic integer types for simplicity
     //public int Id { get; set; }
-    public Guid Id { get; set; } = Guid.NewGuid();
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; } 
 
+    [NotMapped]
     private readonly List<BaseEvent> _domainEvents = new();
 
     [NotMapped]
--- src/Domain/Entities/Account.cs
+++ src/Domain/Entities/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +9,25 @@ using System.Threading.Tasks;
 namespace sp26se058_3dprintshop_be.Domain.Entities;
 public class Account : BaseAuditableEntity // Kế thừa để có CreatedBy, LastModifiedBy
 {
-    public string Username { get; set; } = string.Empty;
-    public string Fullname { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    [MaxLength(20)]
+    [Required]
+    public string Username { get; set; } = null!;
+    [MaxLength(40)]
+    public string Fullname { get; set; } = null!;
+    [MaxLength(40)]
+    public string Email { get; set; } = null!;
+    [MaxLength(255)]
     public string? Profile_Image_URL { get; set; }
-    public string Contact_Phone { get; set; } = string.Empty;
+    [MaxLength(15)]
+    public string? Contact_Phone { get; set; } 
+    [MaxLength(15)]
     public string? Zalo_Phone { get; set; }
-    public string Password_Hash { get; set; } = string.Empty;
+    [MaxLength(255)]
+    public string Password_Hash { get; set; } = null!;
     public bool Is_active { get; set; } = true;
 
     // Navigation properties (Mối quan hệ 1:1)
-    //public virtual Staff? Staff { get; set; }
-    //public virtual Customer? Customer { get; set; }
-    //public virtual Manager? Manager { get; set; }
+    public virtual Staff? Staff { get; set; }
+    public virtual Customer? Customer { get; set; }
+    public virtual Manager? Manager { get; set; }
 }
