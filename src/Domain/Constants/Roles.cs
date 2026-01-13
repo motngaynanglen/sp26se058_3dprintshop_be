bc0bbc8d6f69e9e45ac1324c7c4bb13e9773b3e0
bachdoan-sa <bach.doansy@gmail.com>
2026-01-14 01:23:36 +0700

    delete net. identity; comment template todos entity

--- src/Domain/Constants/Roles.cs
+++ src/Domain/Constants/Roles.cs
@@ -3,4 +3,7 @@
 public abstract class Roles
 {
     public const string Administrator = nameof(Administrator);
+    public const string Manager = nameof(Manager);
+    public const string Staff = nameof(Staff);
+    public const string Customer = nameof(Customer);
 }
