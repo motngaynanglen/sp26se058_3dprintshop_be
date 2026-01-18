fe944ed697a8248778babcff60660319f781b9a7
bachdoan-sa <bach.doansy@gmail.com>
2026-01-18 23:17:09 +0700

    update transaction db format

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -5,26 +5,12 @@ namespace sp26se058_3dprintshop_be.Domain.Entities;
 
 public class Transaction : BaseAuditableEntity
 {
-    [Required]
     public Guid InvoiceId { get; set; }
-
-    [ForeignKey(nameof(InvoiceId))]
+    public decimal Amount { get; set; }
+    public string? PaymentMethod { get; set; } // MoMo, VNPAY, BankTransfer, Cash
+    public string? ExternalTransactionId { get; set; } // Mã giao dịch từ cổng thanh toán (ví dụ: mã tham chiếu MoMo)
+    public string? Note { get; set; }
+    public string TransactionStatus { get; set; } = "PENDING"; // Pending, Success, Failed
     public virtual Invoice Invoice { get; set; } = null!;
 
-    //  [Column(TypeName = "decimal(18,2)")]
-    public decimal Amount { get; set; }
-
-    [MaxLength(50)]
-    public string? PaymentMethod { get; set; } // MoMo, VNPAY, BankTransfer, Cash
-
-    [MaxLength(100)]
-    public string? ExternalTransactionId { get; set; } // Mã giao dịch từ cổng thanh toán (ví dụ: mã tham chiếu MoMo)
-
-    public string? Note { get; set; }
-
-    [Required]
-    [MaxLength(20)]
-    public string TransactionStatus { get; set; } = "PENDING"; // Pending, Success, Failed
-
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
