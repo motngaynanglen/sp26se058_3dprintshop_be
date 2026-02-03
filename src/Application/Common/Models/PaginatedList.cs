c097f87b96728d57fd8572f0e669332550f96b5b
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-02-03 16:27:07 +0700

    update crud account/ paging model

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -3,10 +3,13 @@
 public class PaginatedList<T>
 {
     public IReadOnlyCollection<T> Items { get; }
+    public PaginationMetadata Metadata { get; }
+
+    /* Code cũ của template
     public int PageNumber { get; }
     public int TotalPages { get; }
     public int TotalCount { get; }
-
+    
     public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
     {
         PageNumber = pageNumber;
@@ -18,6 +21,12 @@ public class PaginatedList<T>
     public bool HasPreviousPage => PageNumber > 1;
 
     public bool HasNextPage => PageNumber < TotalPages;
+    */
+    public PaginatedList(IReadOnlyCollection<T> items, int count, int pageNumber, int pageSize)
+    {
+        Items = items;
+        Metadata = new PaginationMetadata(count, pageNumber, pageSize);
+    }
 
     public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
     {
@@ -25,5 +34,21 @@ public class PaginatedList<T>
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
 
         return new PaginatedList<T>(items, count, pageNumber, pageSize);
+    }
+}
+public class PaginationMetadata 
+{
+    public int PageNumber { get; init; }
+    public int TotalPages { get; init; }
+    public int TotalCount { get; init; }
+    public int PageSize { get; init; }    // Cái này gửi về chỉ để đối chiếu là chủ yếu, không quan trọng
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+    public PaginationMetadata(int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
     }
 }
