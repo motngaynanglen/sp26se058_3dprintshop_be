34e7a021a415b59ebd2e9e4e9020d27885e4ded2
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-02-09 06:38:13 +0700

    add status definition

---
+++ src/Domain/Constants/Statuses/InvoiceStatuses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sp26se058_3dprintshop_be.Domain.Constants.Statuses;
+public static class InvoiceStatuses
+{
+    public const string Unpaid = "UNPAID";
+    public const string Paid = "PAID";
+    // public const string PartiallyPaid = "PARTIALLY_PAID"; // Cân nhắc thêm vào sau
+
+    public static readonly List<StatusDefinition> All = new()
+    {
+        new(Unpaid, "Chưa thanh toán", "#F44336", "Hóa đơn chưa được thực hiện thanh toán."),
+        new(Paid, "Đã thanh toán", "#4CAF50", "Hóa đơn đã thanh toán đầy đủ 100%."),
+        /*
+        new(PartiallyPaid, "Thanh toán một phần", "#FF9800", "Mới nhận một phần tiền hoặc tiền đặt cọc.")
+        */
+    };
+}
