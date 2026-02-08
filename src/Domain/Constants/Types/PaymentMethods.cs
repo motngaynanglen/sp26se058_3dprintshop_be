527aac0f335ec00583b63c4d2c133b771e272279
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-02-09 06:38:13 +0700

    add status definition

---
+++ src/Domain/Constants/Types/PaymentMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sp26se058_3dprintshop_be.Domain.Constants.Types;
+public static class PaymentMethods
+{
+    public const string BankTransfer = "BANK_TRANSFER";
+    //public const string MoMo = "MOMO"; // Cân nhắc thêm vào sau
+    //public const string Cash = "CASH";
+    public const string VNPAY = "VNPAY"; 
+
+    public static readonly List<StatusDefinition> All = new()
+    {
+        new(BankTransfer, "Chuyển khoản ngân hàng", "#00468C", "Thanh toán qua số tài khoản ngân hàng chính thức."),
+        new(VNPAY, "Ví điện tử VNPAY", "#A50064", "Thanh toán nhanh qua ứng dụng VNPAY."),
+        //new(Cash, "Tiền mặt", "#4CAF50", "Thanh toán trực tiếp tại cửa hàng hoặc khi nhận hàng."),
+    };
+}
