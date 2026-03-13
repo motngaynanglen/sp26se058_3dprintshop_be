3d10443200ab6c9bd1b0f1450070523e7641978c
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-13 18:20:40 +0700

    them get me, update, fix mo ta

---
+++ src/Application/Accounts/Queries/GetAccountMineDetailQuery.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sp26se058_3dprintshop_be.Application.Common.Mappings;
+using sp26se058_3dprintshop_be.Application.Common.Models;
+using sp26se058_3dprintshop_be.Application.Common.Security;
+using sp26se058_3dprintshop_be.Domain.Constants;
+
+namespace sp26se058_3dprintshop_be.Application.Accounts.Queries.GetAccountsWithPagination;
+public class GetAccountMineDetailQuery : IRequest<AccountDto>
+{
+    public class GetAccountMineDetailQueryHandler : IRequestHandler<GetAccountMineDetailQuery, AccountDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IUser _user;
+
+        public GetAccountMineDetailQueryHandler(IApplicationDbContext context, IMapper mapper, IUser user)
+        {
+            _context = context;
+            _mapper = mapper;
+            _user = user;
+        }
+        public async Task<AccountDto> Handle(GetAccountMineDetailQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _user.Id.ToGuid();
+            if (userId == Guid.Empty)
+            {
+                throw new Exception("Hãy đăng nhập.");
+            }
+            
+            var query = _context.Accounts.AsNoTracking();
+               
+            var account = await query.ProjectTo<AccountDto>(_mapper.ConfigurationProvider) //Mapper tự tính toán field Role
+                .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+            if (account == null) throw new Exception("Tài khoản không tồn tại");
+
+            return account;
+        }
+    }
+}
---
+++ src/Application/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sp26se058_3dprintshop_be.Application.Common.Extensions;
+public static class StringExtensions
+{
+    /// <summary>
+    /// Chuyển đổi string sang Guid, nếu lỗi trả về Guid.Empty (0000...)
+    /// </summary>
+    public static Guid ToGuid(this string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return Guid.Empty;
+        }
+
+        return Guid.TryParse(value, out var result) ? result : Guid.Empty;
+    }
+}
