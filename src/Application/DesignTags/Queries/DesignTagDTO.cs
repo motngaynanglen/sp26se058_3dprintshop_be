793977744ae1522e51c41ddac28a9d80059e2757
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-21 03:06:04 +0700

    Update DesignTagDTO.cs

--- src/Application/DesignTags/Queries/DesignTagDTO.cs
+++ src/Application/DesignTags/Queries/DesignTagDTO.cs
@@ -15,8 +15,8 @@ public class DesignTagDTO
     {
         public Mapping()
         {
-            CreateMap<Domain.Entities.DesignTag, DesignTagDTO>()
-                .ForMember(t => t.Name, opt => opt.MapFrom(m => m.ConceptTag != null ? m.ConceptTag.Name : string.Empty));
+            CreateMap<Domain.Entities.DesignTag, DesignTagDTO>();
+                //.ForMember(t => t.Name, opt => opt.MapFrom(m => m.ConceptTag != null ? m.ConceptTag.Name : string.Empty));
         }
     }
 }
