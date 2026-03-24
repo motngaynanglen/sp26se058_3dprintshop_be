fa39a6c6ca1f3cd45d42a31bff025d95e696196a
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-25 00:59:12 +0700

    Update Feedback.cs

--- src/Domain/Entities/Feedback.cs
+++ src/Domain/Entities/Feedback.cs
@@ -5,6 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace sp26se058_3dprintshop_be.Domain.Entities;
-internal class Feedback
+public class Feedback : BaseAuditableEntity
 {
+    public Guid CustomerId { get; set; }
+    public Guid DesignTemplateId { get; set; }
+    public Guid OrderItemId { get; set; }
+    public int Rating { get; set; }
+    public string? Comment { get; set; }
+    public string? StaffReply { get; set; }
+    public bool IsHidden { get; set; } = false;
+
+    // Navigation Properties
+    public virtual Customer Customer { get; set; } = null!;
+    public virtual DesignTemplate DesignTemplate { get; set; } = null!;
+    public virtual OrderItem OrderItem { get; set; } = null!;
+    public virtual ICollection<FeedbackImage> FeedbackImages { get; set; } = new List<FeedbackImage>();
 }
