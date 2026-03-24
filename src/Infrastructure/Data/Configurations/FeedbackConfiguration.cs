ed7b9920b87086f92af423ebda6f01cbd1ba5d19
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-25 00:54:09 +0700

    add entity feedback

---
+++ src/Infrastructure/Data/Configurations/FeedbackConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using sp26se058_3dprintshop_be.Domain.Entities;
+
+namespace sp26se058_3dprintshop_be.Infrastructure.Data.Configurations;
+public class FeedbackConfiguration : IEntityTypeConfiguration<Feedback>
+{
+    public void Configure(EntityTypeBuilder<Feedback> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.Rating)
+            .IsRequired();
+
+        // Thêm Check Constraint cho Rating (1-5) ở mức Database
+        builder.ToTable(t => t.HasCheckConstraint("CK_Feedback_Rating", "[Rating] >= 1 AND [Rating] <= 5"));
+
+        builder.Property(f => f.Comment)
+            .HasMaxLength(2000);
+
+        builder.Property(f => f.StaffReply)
+            .HasMaxLength(2000);
+
+        // Mỗi OrderItem chỉ được feedback một lần duy nhất
+        builder.HasIndex(f => f.OrderItemId).IsUnique();
+
+        // Relationships
+        builder.HasOne(f => f.Customer)
+            .WithMany() // Nếu Customer không có List<Feedback>
+            .HasForeignKey(f => f.CustomerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(f => f.DesignTemplate)
+            .WithMany()
+            .HasForeignKey(f => f.DesignTemplateId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(f => f.FeedbackImages)
+            .WithOne(fi => fi.Feedback)
+            .HasForeignKey(fi => fi.FeedbackId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
