e9ab04a17c9191c775947c8d47c64be4c0a1d02b
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-30 01:20:04 +0700

    Thêm designtag api create, update, delete

---
+++ src/Application/DesignTags/Commands/UpdateDesignTagCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sp26se058_3dprintshop_be.Domain.Entities;
+
+namespace sp26se058_3dprintshop_be.Application.DesignTags.Commands;
+public record UpdateDesignTagCommand : IRequest<UpdateDesignTagCommand>
+{
+    [DefaultValue("00000000-0000-0000-0000-000000000001")]
+    public Guid Id { get; set; }
+    [DefaultValue(false)]
+    public bool IsMainTag { get; set; }
+}
+public class UpdateDesignTagCommandHandler : IRequestHandler<UpdateDesignTagCommand, UpdateDesignTagCommand>
+{
+    private readonly IApplicationDbContext _context;
+    public UpdateDesignTagCommandHandler(IApplicationDbContext context) => _context = context;
+
+    public async Task<UpdateDesignTagCommand> Handle(UpdateDesignTagCommand request, CancellationToken ct)
+    {
+        var entity = await _context.DesignTags.FindAsync(request.Id);
+        if (entity == null) throw new Exception("Tag không tồn tại.");
+
+        if (request.IsMainTag)
+        {
+            // Tắt các MainTag khác của cùng Template đó
+            var others = await _context.DesignTags
+                .Where(x => x.DesignTemplateId == entity.DesignTemplateId && x.Id != entity.Id)
+                .ToListAsync(ct);
+            foreach (var tag in others) tag.IsMainTag = false;
+        }
+
+        entity.IsMainTag = request.IsMainTag;
+        await _context.SaveChangesAsync(ct);
+        return request;
+    }
+}
