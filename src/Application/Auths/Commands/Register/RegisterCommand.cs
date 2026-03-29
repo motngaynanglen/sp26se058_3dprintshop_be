4f2e89d6d4d415f212244e98cfc9831502d0b7aa
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-30 01:00:11 +0700

    fix bug

--- src/Application/Auths/Commands/Register/RegisterCommand.cs
+++ src/Application/Auths/Commands/Register/RegisterCommand.cs
@@ -20,7 +20,7 @@ public record RegisterCommand : IRequest<bool>
     [DefaultValue("VanA@gmail.com")]
     public string Email { get; init; } = null!;
     [DefaultValue("0777777777")]
-    public string? PhoneNumber { get; init; }
+    public string? ContactPhone { get; init; }
 }
 
 public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
@@ -31,7 +31,7 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
         RuleFor(v => v.Password).NotEmpty().MinimumLength(6).WithMessage("Mật khẩu tối thiểu 6 ký tự.");
         RuleFor(v => v.Fullname).NotEmpty().MaximumLength(100);
         RuleFor(v => v.Email).NotEmpty().EmailAddress().WithMessage("Định dạng email không hợp lệ.");
-        RuleFor(v => v.PhoneNumber).NotEmpty();
+        RuleFor(v => v.ContactPhone).NotEmpty();
     }
 }
 
@@ -71,6 +71,7 @@ public class RegisterCommandHandler : IRequestHandler<RegisterCommand, bool>
             PasswordHash = passwordHash,
             Fullname = request.Fullname,
             Email = request.Email,
+            ContactPhone = request.ContactPhone,
         };
 
         // 4. Khởi tạo thông tin Customer liên kết với Account
