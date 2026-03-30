4130ada7dcd59d1d51643dc3c20efa0e78aa22d3
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-03-30 19:06:34 +0700

    Update CheckoutCommand.cs

--- src/Application/Orders/Commands/CheckoutCommand.cs
+++ src/Application/Orders/Commands/CheckoutCommand.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using Org.BouncyCastle.Asn1.Ocsp;
 using sp26se058_3dprintshop_be.Application.Common.Interfaces;
 using sp26se058_3dprintshop_be.Application.Common.Security;
 using sp26se058_3dprintshop_be.Domain.Constants;
+using sp26se058_3dprintshop_be.Domain.Constants.Types;
 using sp26se058_3dprintshop_be.Domain.Entities;
 
 namespace sp26se058_3dprintshop_be.Application.Orders.Commands;
@@ -20,6 +22,8 @@ public record CheckoutCommand : IRequest<Guid>
     //public Guid ShippingMethodId { get; init; }
     //[DefaultValue("ONLINE")]
     //public string? PaymentMethod { get; init; } // MoMo, BankTransfer
+    [DefaultValue("ORDER")]
+    public string SourceType { get; init; } = null!;
     public string? Note { get; init; }
     public List<CheckoutItemRequest> Items { get; init; } = new();
 }
@@ -41,7 +45,7 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
         {
             throw new Exception("Chỉ có khách hàng mới có thể dùng phương thức này.");
         }
-        var order = await CreateOrderAsync(customer.Id, request.Items, cancellationToken);
+        var order = await CreateOrderAsync(customer.Id, request, cancellationToken);
         var shipment = CreateShipment(order.Id, request.ShippingAddressId, 0);
         var invoice = CreateInvoice(order);
 
@@ -49,7 +53,7 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
 
         return order.Id;
     }
-    private async Task<Order> CreateOrderAsync(Guid customerId, List<CheckoutItemRequest> items, CancellationToken ct)
+    private async Task<Order> CreateOrderAsync(Guid customerId, CheckoutCommand request, CancellationToken ct)
     {
         var order = new Order
         {
@@ -60,10 +64,10 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
             TotalPrice = 0
         };
 
-        foreach (var itemReq in items)
+        foreach (var itemReq in request.Items)
         {
             decimal unitPrice = 0;
-            if (itemReq.SourceType == "ORDER" && itemReq.DesignVariantId.HasValue)
+            if (request.SourceType == "ORDER" && itemReq.DesignVariantId.HasValue)
             {
                 // 1. Lấy thông tin biến thể sản phẩm kèm theo Lock (nếu cần)
                 var variant = await _context.DesignVariants
@@ -92,7 +96,7 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
                     // Lưu ID của Order để sau này biết món hàng này xuất cho đơn nào
                     ReferenceId = order.Id,
                     Quantity = -itemReq.Quantity, // Xuất kho để số âm
-                    Type = "OrderOut", // Khớp với enum/string bạn đã định nghĩa
+                    Type = InventoryTransactionTypes.OrderOut, 
                     Note = $"Khách hàng đặt hàng từ đơn: {order.Code}",
                     // StaffId = null vì đây là hệ thống tự động xuất khi khách mua
                 };
@@ -105,7 +109,7 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
             var orderItem = new OrderItem
             {
                 Id = Guid.NewGuid(),
-                SourceType = itemReq.SourceType,
+                SourceType = request.SourceType,
                 DesignVariantId = itemReq.DesignVariantId,
                 QuantityOrdered = itemReq.Quantity,
                 UnitPrice = unitPrice,
@@ -147,103 +151,10 @@ public class CheckoutCommandHandler : IRequestHandler<CheckoutCommand, Guid>
         _context.Invoices.Add(invoice);
         return Task.FromResult(invoice);
     }
-    //public async Task<Guid> Handle(CheckoutCommand request, CancellationToken cancellationToken)
-    //{
-    //    var customerId = _user.Id.ToGuid(); // Lấy ID từ Token
-    //    //using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
-    //    var order = new Order
-    //    {
-    //        Id = Guid.NewGuid(),
-    //        Code = $"ORD-{DateTime.Now.Ticks}",// Chưa impliment code
-    //        CustomerId = customerId,
-    //        OrderStatus = "PENDING",
-    //        Priority = 0,
-    //        TotalPrice = 0 // Sẽ cộng dồn sau
-    //    };
-    //    decimal totalAmount = 0;
-    //    // 3. Xử lý Order Items
-    //    foreach (var itemReq in request.Items)
-    //    {
-    //        decimal unitPrice = 0;
-
-    //        // Logic lấy giá tùy theo loại hàng
-    //        if (itemReq.SourceType == "ORDER" && itemReq.DesignVariantId.HasValue)
-    //        {
-    //            var variant = await _context.DesignVariants
-    //                .FindAsync(new object[] { itemReq.DesignVariantId.Value }, cancellationToken);
-
-    //            if (variant == null || variant.StockQuantity < itemReq.Quantity && !variant.IsAllowPreOrder)
-    //                throw new Exception($"Sản phẩm {variant?.Name} không đủ hàng.");
-
-    //            unitPrice = variant.Price;
-    //        }
-    //        //else if (itemReq.SourceType == "DesignService" && itemReq.DesignWorkId.HasValue)
-    //        //{
-    //        //    // Logic lấy giá từ gói thiết kế (ServicePackage) liên quan
-    //        //    var designWork = await _context.DesignWorks.FindAsync(itemReq.DesignWorkId.Value);
-    //        //    // Giả sử unitPrice lấy từ gói dịch vụ...
-    //        //}
-
-    //        var orderItem = new OrderItem
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            //OrderId = order.Id,
-    //            SourceType = itemReq.SourceType,
-    //            DesignVariantId = itemReq.DesignVariantId,
-    //            //DesignWorkId = itemReq.DesignWorkId,
-    //            QuantityOrdered = itemReq.Quantity,
-    //            UnitPrice = unitPrice,
-    //            TotalPrice = unitPrice * itemReq.Quantity,
-    //            FulfillmentStatus = "Pending",
-
-    //            Order = order
-    //        };
-
-    //        totalAmount += orderItem.TotalPrice;
-    //        _context.OrderItems.Add(orderItem);
-    //        // 4. Tính toán phí Ship & Tổng tiền
-    //        //var shippingMethod = await _context.ShippingMethods.FindAsync(request.ShippingMethodId);
-    //        //decimal shippingFee = shippingMethod?.BaseFee ?? 0; // Giả định có BaseFee
-
-    //        //order.TotalPrice = totalAmount + shippingFee;
-    //        //// Quy tắc: Cọc 50%
-    //        //order.DepositAmount = order.TotalPrice * 0.5m;
-    //        //order.PaidAmount = 0;
-
-    //        _context.Orders.Add(order);
-
-    //        // 5. Tạo Shipment
-    //        var shipment = new Shipment
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            OrderId = order.Id,
-    //            ShippingAddressId = request.ShippingAddressId,
-    //            //ShippingMethodId = request.ShippingMethodId,
-    //            ShippingFee = 0,
-    //            ShipmentStatus = "PENDING"
-    //        };
-    //        _context.Shipments.Add(shipment);
-    //        // 6. Tạo Invoice
-    //        var invoice = new Invoice
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            OrderId = order.Id,
-    //            InvoiceCode = $"INV-{order.Id}",
-    //            TotalAmount = order.TotalPrice,
-    //            PaymentStatus = "UNPAID",
-    //        };
-    //        _context.Invoices.Add(invoice);
-    //        await _context.SaveChangesAsync(cancellationToken);
-    //        return order.Id;
-    //    }
-    //    //await transaction.RollbackAsync(cancellationToken);
-    //    throw new Exception("Lỗi add");
-    //}
 }
 public record CheckoutItemRequest
 {
-    [DefaultValue("ORDER")]
-    public string SourceType { get; init; } = null!;
+
     [DefaultValue("00000000-0000-0000-0000-000000000000")]
     public Guid? DesignVariantId { get; init; }
     //public Guid? DesignWorkId { get; init; }
