226985b75fc8d95cb5fd57c33be3021016ac1915
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-04-06 01:42:12 +0700

    them update, query, delete service option

---
+++ src/Application/ServiceOptions/Queries/GetServiceOptionsQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sp26se058_3dprintshop_be.Application.ServicePackages.Queries;
+using sp26se058_3dprintshop_be.Domain.Entities;
+
+namespace sp26se058_3dprintshop_be.Application.ServiceOptions.Queries;
+public record GetServiceOptionsQuery : IRequest<List<ServiceOption>>;
+
+public class GetServiceOptionsQueryHandler : IRequestHandler<GetServiceOptionsQuery, List<ServiceOption>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    public GetServiceOptionsQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    } 
+
+    public async Task<List<ServiceOption>> Handle(GetServiceOptionsQuery request, CancellationToken ct)
+    {
+        return await _context.ServiceOptions
+            .AsNoTracking()
+            .OrderBy(x => x.OptionType)
+            .ProjectTo<ServiceOption>(_mapper.ConfigurationProvider)
+            .ToListAsync(ct);
+    }
+}
