6de85fc13ddf90b5dcbb1f5d8242d349c0e0b569
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-04-12 17:29:50 +0700

    sửa migration FixShipmentRelationship

---
+++ src/Infrastructure/Migrations/20260412102813_FixShipmentRelationship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sp26se058_3dprintshop_be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixShipmentRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
