404f541311ee69199aa901ce713784019ad2dd63
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-04-13 10:19:40 +0700

    Update DependencyInjection.cs

--- src/Web/DependencyInjection.cs
+++ src/Web/DependencyInjection.cs
@@ -43,7 +43,7 @@ public static class DependencyInjection
                     .SelectMany(x => x.Errors)
                     .Select(x => x.ErrorMessage)
                     .FirstOrDefault();
-
+                Console.WriteLine($"[Factory Log]: Model Binding Error - {error}");
                 // Ném lỗi này ra để Middleware Exception bắt được
                 throw new BadHttpRequestException($"Lỗi định dạng: {error}");
             };
