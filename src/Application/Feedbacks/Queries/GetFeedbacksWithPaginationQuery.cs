b1cdb35f5fb8784332667a05f0095da0736e8653
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-04-14 18:05:56 +0700

    thêm và bổ xung trả lỗi, phản hồi feedback

--- src/Application/Feedbacks/Queries/GetFeedbacksWithPaginationQuery.cs
+++ src/Application/Feedbacks/Queries/GetFeedbacksWithPaginationQuery.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using sp26se058_3dprintshop_be.Application.Common.Mappings;
 using sp26se058_3dprintshop_be.Application.Common.Models;
+using sp26se058_3dprintshop_be.Domain.Constants;
 
 namespace sp26se058_3dprintshop_be.Application.Feedbacks.Queries;
+[Authorize(Roles =  Roles.MANAGER + "," + Roles.STAFF)]
 public class GetFeedbacksWithPaginationQuery : PaginationRequest, IRequest<PaginatedList<FeedbackDTO>>
 {
     public int? Rating { get; init; }
@@ -26,17 +28,20 @@ public class GetFeedbacksWithPaginationHandler : IRequestHandler<GetFeedbacksWit
 
     public async Task<PaginatedList<FeedbackDTO>> Handle(GetFeedbacksWithPaginationQuery request, CancellationToken ct)
     {
-        var query = _context.Feedbacks.AsQueryable();
-        if (request.IsDeleted.HasValue)
-            query = query.IgnoreQueryFilters();
+        var query = request.IsDeleted == true
+            ? _context.Feedbacks.IgnoreQueryFilters().Where(x => x.Deleted != null)
+            : _context.Feedbacks.AsNoTracking();
         // Filter theo số sao
         if (request.Rating.HasValue)
+        {
             query = query.Where(x => x.Rating == request.Rating);
+        }
 
         // Filter theo trạng thái ẩn/hiện
         if (request.IsHidden.HasValue)
+        {
             query = query.Where(x => x.IsHidden == request.IsHidden);
-
+        }
         return await query
             .OrderByDescending(x => x.Created)
             .ProjectTo<FeedbackDTO>(_mapper.ConfigurationProvider)
