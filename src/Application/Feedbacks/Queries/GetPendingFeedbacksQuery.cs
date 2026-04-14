530951c9d09997e2fa860727e842cc5ae0e20b5c
bachdoan-sa <125141982+bachdoan-sa@users.noreply.github.com>
2026-04-14 16:02:40 +0700

    thêm authorize cho order gets

--- src/Application/Feedbacks/Queries/GetPendingFeedbacksQuery.cs
+++ src/Application/Feedbacks/Queries/GetPendingFeedbacksQuery.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using sp26se058_3dprintshop_be.Application.Common.Mappings;
 using sp26se058_3dprintshop_be.Application.Common.Models;
+using sp26se058_3dprintshop_be.Domain.Constants;
+using sp26se058_3dprintshop_be.Domain.Constants.Statuses;
 
 namespace sp26se058_3dprintshop_be.Application.Feedbacks.Queries;
+[Authorize(Roles = Roles.CUSTOMER)]
 public class GetPendingFeedbacksQuery : PaginationRequest, IRequest<PaginatedList<PendingFeedbackDTO>>
 {
     public class GetPendingFeedbacksQueryHandler : IRequestHandler<GetPendingFeedbacksQuery, PaginatedList<PendingFeedbackDTO>>
@@ -24,12 +27,20 @@ public class GetPendingFeedbacksQuery : PaginationRequest, IRequest<PaginatedLis
         public async Task<PaginatedList<PendingFeedbackDTO>> Handle(GetPendingFeedbacksQuery request, CancellationToken ct)
         {
             var userId = _user.Id.ToGuid();
-            return await _context.OrderItems
-                            .Where(oi => oi.Order.CustomerId == userId
-                                      && oi.Order.OrderStatus == "COMPLETED"
-                                      && !_context.Feedbacks.Any(f => f.OrderItemId == oi.Id))
-                            .ProjectTo<PendingFeedbackDTO>(_mapper.ConfigurationProvider)
-                            .PaginatedListAsync(request.PageNumber, request.PageSize);
+            var query = _context.OrderItems
+                .AsNoTracking()
+                .Where(oi => oi.Order.Customer.AccountId == userId
+                        // 1. Order tổng phải hoàn thành
+                        && oi.Order.OrderStatus == OrderStatuses.Completed
+                        // 2. Quan trọng: Item đó phải thực sự đã giao/hoàn tất (tùy status của bạn)
+                        // Nếu item vẫn PENDING thì chưa cho feedback
+                        // && oi.FulfillmentStatus == FulfillmentStatuses.Fulfilled 
+
+                        // 3. Item này chưa từng có feedback (Dùng !Any)
+                        && !_context.Feedbacks.Any(f => f.OrderItemId == oi.Id));
+            return await query
+                .ProjectTo<PendingFeedbackDTO>(_mapper.ConfigurationProvider)
+                .PaginatedListAsync(request.PageNumber, request.PageSize);
         }
     }
 }
